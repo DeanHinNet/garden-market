2336a7266e168c2a954f555782628a7b0ae064a9
dean <hindrough@gmail.com>
2018-11-30 15:53:16 -0500

    Update server.

--- build/server/index.js
+++ build/server/index.js
@@ -1,8 +1,11 @@
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
-const port = 8080;
+const port = 8820;
+
+const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/dist/'));
 
 app.listen(port, ()=>{
     console.log(`Pool listening on ${port}`);
